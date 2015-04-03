0ea9ea6517b725a93f15fb318d3a6d9a242520d6
Nikki <nikki.corvers@gmail.com>
2015-04-03 13:28:12 +0200

    sharedlayout - appname added

---
+++ LimburgseWolvenApp.Tests/SetupTest.cs
@@ -0,0 +1,42 @@
+﻿using LimburgseWolvenApp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimburgseWolvenApp.Tests
+{
+    [TestClass]
+    public class SetupTest
+    {
+        private ApplicationDbContext context = new ApplicationDbContext();
+
+        [TestInitialize]
+        public void TestInitializer()
+        {
+            context.Personen.Delete();
+            context.Reservaties.Delete();
+            context.Groepen.Delete();
+            context.Bewoners.Delete();
+            context.Gestorvenen.Delete();
+            context.UserDorpen.Delete();
+            context.Zwervers.Delete();
+
+        }
+
+        [TestMethod]
+        public void AlsEr0InschrijvingenZijnErGeenBewoners()
+        {
+            // Arrange
+            
+
+            // Act
+            
+
+            // Assert
+            
+        }
+    }
+}
---
+++ LimburgseWolvenApp/Models/SetupSpel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LimburgseWolvenApp.Models
+{
+    public class SetupSpel
+    {
+        public void setupRondes()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var aantalDeelnemers = context.Personen.Count();
+        }
+    }
+}
